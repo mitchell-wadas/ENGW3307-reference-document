832118d1d0190f61e97baf0e4124139607655739
Mitchell Wadas <mitchell@Mitchells-MacBook-Pro.local>
2023-05-22 16:29:57 -0400

    Add eval modus_ponens

--- modus_ponens.lean
+++ modus_ponens.lean
@@ -3,6 +3,14 @@ variables p q r : Prop
 lemma modus_ponens : p → (p → q) → q :=
     λ hp : p, λ hpq : p → q, hpq hp 
 
+variable hp : p
+variable hpq : p → q
+
+#check modus_ponens             -- ∀ (p q : Prop), p → (p → q) → q
+#check modus_ponens p q         -- p → (p → q) → q
+#check modus_ponens p q hp      -- (p → q) → q
+#check modus_ponens p q hp hpq  -- q
+
 lemma modus_ponens1 : p → (p → q) → q :=
 begin
     intro hp,
